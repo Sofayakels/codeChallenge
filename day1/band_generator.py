825a6f3ffa7f81f2bdc67cb241ad6311f8c5ca95
Chidubem <110422191+Sofayakels@users.noreply.github.com>
2024-05-10 14:18:17 +0100

    test

---
+++ day1/band_generator.py
@@ -0,0 +1,8 @@
+print("Welcome to Band Name Generator!!")
+
+favColour = input("Enter your favourite colour: \n").lower()
+birthPlace = input("Enter your place of birth: \n").lower()
+
+bandName = favColour + birthPlace
+
+print(f"The name of your band is, {bandName}")
