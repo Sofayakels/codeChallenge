7f47e80501fcc2278a6b7b79eb92a43452890140
Chidubem <110422191+Sofayakels@users.noreply.github.com>
2024-05-10 15:29:53 +0100

    Day2 completed

---
+++ day2/addTwo_digits.py
@@ -0,0 +1,10 @@
+print(" Addition of two digits")
+
+two_digit = (input("Create a two digit number: "))
+
+digit1 = int(two_digit[0])
+digit2 = int(two_digit[1])
+
+outcome = digit1 + digit2
+
+print(f"The sum total is: {outcome}")
